f178479a536749f029f365ece503c51277a19354
KK Barrows <58014675+Barrowsk@users.noreply.github.com>
2023-03-16 10:04:20 -0400

    Add project files

---
+++ BB Quotes/BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by KK Barrows on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LOTR Converter/LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by KK Barrows on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
